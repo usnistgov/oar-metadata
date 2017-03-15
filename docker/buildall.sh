358dcc2bd6db5a5e48d22e06fac3177239ccc751
Ray Plante <raymond.plante@nist.gov>
2017-03-15 15:54:47 -0400

    updated docker scripts to represent spin-off package

--- docker/buildall.sh
+++ docker/buildall.sh
@@ -5,7 +5,7 @@ execdir=`dirname $0`
 [ "$execdir" = "" -o "$execdir" = "." ] && execdir=$PWD
 set -ex
 
-docker build -t oarpdr/pymongo $execdir/pymongo
-docker build -t oarpdr/jq $execdir/jq
-docker build -t oarpdr/ejsonschema $execdir/ejsonschema
-docker build -t oarpdr/mdtests $execdir/mdtests
+docker build -t oarmeta/pymongo $execdir/pymongo
+docker build -t oarmeta/jq $execdir/jq
+docker build -t oarmeta/ejsonschema $execdir/ejsonschema
+docker build -t oarmeta/mdtests $execdir/mdtests
--- docker/run.sh
+++ docker/run.sh
@@ -3,7 +3,7 @@
 prog=`basename $0`
 execdir=`dirname $0`
 [ "$execdir" = "" -o "$execdir" = "." ] && execdir=$PWD
-codedir=`(cd $execdir/../.. > /dev/null 2>&1; pwd)`
+codedir=`(cd $execdir/.. > /dev/null 2>&1; pwd)`
 
 set -e
 
