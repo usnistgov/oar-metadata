f49a5de278998e27669740c50c7485c0785e2bd2
Ray Plante <raymond.plante@nist.gov>
2017-05-30 16:57:55 -0400

    ensure exit code from mdtests container

--- docker/mdtests/entrypoint.sh
+++ docker/mdtests/entrypoint.sh
@@ -3,28 +3,29 @@
 mongod_ctl=/usr/local/bin/mongod_ctl.sh
 [ "$1" = "" ] && exec /bin/bash
 
-while [ "$1" != "" ]; do
-    case "$1" in
-        testall)
-            [ -x $mongo_ctl ] && $mongod_ctl start && sleep 1
-            scripts/testall.py
-            echo $? > testall.exit
-            ;;
-        install)
-            scripts/install.sh
-            ;;
-        testshell)
-            [ -x $mongo_ctl ] && $mongod_ctl start && sleep 1
-            exec /bin/bash
-            ;;
-        shell)
-            exec /bin/bash
-            ;;
-        *)
-            echo Unknown command: $1
-            echo Available commands:  testall install shell
-            ;;
-    esac
-    
-    shift
-done
+
+case "$1" in
+    testall)
+        [ -x $mongo_ctl ] && $mongod_ctl start && sleep 1
+        scripts/testall.py
+        ;;
+    install)
+        scripts/install.sh
+        ;;
+    testshell)
+        [ -x $mongo_ctl ] && $mongod_ctl start && sleep 1
+        exec /bin/bash
+        ;;
+    shell)
+        exec /bin/bash
+        ;;
+    *)
+        echo Unknown command: $1
+        echo Available commands:  testall install shell
+        exit 100
+        ;;
+esac
+
+EXCODE=$?
+echo $EXCODE > $1.exit
+exit $EXCODE
