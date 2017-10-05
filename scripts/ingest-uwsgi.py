f9a76c3bdf281dcf931c5bea0679d171d7a2a873
Ray Plante <raymond.plante@nist.gov>
2017-10-05 12:46:22 -0400

    ingest-uwsgi.py: fix setting or user/pass (pt2)

--- scripts/ingest-uwsgi.py
+++ scripts/ingest-uwsgi.py
@@ -61,8 +61,8 @@ if cfg.get('db_authn') and \
         if confenv:
             rmmconfsrc += '/'+confenv
         rmmcfg = config.resolve_configuration(rmmconfsrc, confserv)
-        acfg['user'] = rmmcfg['oar.mongodb.readwrite.user'],
-        acfg['pass'] = rmmcfg['oar.mongodb.readwrite.password'],
+        acfg['user'] = rmmcfg['oar.mongodb.readwrite.user']
+        acfg['pass'] = rmmcfg['oar.mongodb.readwrite.password']
 
     except Exception, ex:
         raise ConfigurationException("Failed to retrieve Mongo authentication "+
