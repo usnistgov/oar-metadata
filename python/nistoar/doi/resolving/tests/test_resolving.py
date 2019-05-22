a15d6b3b8e3ec3713704c053869144581ddeac27
Ray Plante <raymond.plante@nist.gov>
2019-05-22 13:46:52 -0400

    add Resolver class, use in resolve() function

--- python/nistoar/doi/resolving/tests/test_resolving.py
+++ python/nistoar/doi/resolving/tests/test_resolving.py
@@ -4,6 +4,7 @@ from collections import Mapping
 from nistoar.tests import *
 
 import nistoar.doi.resolving as res
+import nistoar.doi.resolving.common as comm
 from nistoar.doi.resolving import set_client_info
 
 dcdoi = "10.18434/m33x0v"
@@ -36,6 +37,7 @@ class TestResolving(test.TestCase):
         self.assertTrue(isinstance(info, res.CrossrefDOIInfo))
         self.assertIsNotNone(info._data)
         self.assertEqual(info.data['DOI'], crdoi)
+        self.assertEqual(info.client_info.application_version, "testing")
 
     @test.skipIf("doi" not in os.environ.get("OAR_TEST_INCLUDE",""),
                  "kindly skipping doi service checks")
@@ -57,10 +59,38 @@ class TestResolving(test.TestCase):
         except (res.DOIResolverError, res.DOICommunicationError):
             pass
 
-    
+class TestResolver(test.TestCase):
+
+    def test_ctor(self):
+        rslvr = res.Resolver(resolver="ftp:/goober.com/")
+        self.assertEquals(rslvr._resolver, "ftp:/goober.com/")
+        self.assertEquals(rslvr._client_info[1], "testing")
+
+        ci = (comm._client_info[0], "testing2", comm._client_info[2],
+              comm._client_info[3])
         
+        rslvr = res.Resolver(ci)
+        self.assertEquals(rslvr._resolver, "https://doi.org/")
+        self.assertEquals(rslvr._client_info[1], "testing2")
         
-    
+        rslvr = res.Resolver()
+        self.assertEquals(rslvr._resolver, "https://doi.org/")
+        self.assertEquals(rslvr._client_info[1], "testing")
+
+
+    @test.skipIf("doi" not in os.environ.get("OAR_TEST_INCLUDE",""),
+                 "kindly skipping doi service checks")
+    def test_cr_resolve(self):
+        ci = (comm._client_info[0], "testing2", comm._client_info[2],
+              comm._client_info[3])
+        
+        rslvr = res.Resolver(ci)
+        info = rslvr.resolve(crdoi)
+        self.assertEqual(info.client_info.application_version, "testing2")
+        self.assertEqual(info.source, "Crossref")
+        self.assertTrue(isinstance(info, res.CrossrefDOIInfo))
+        self.assertIsNotNone(info._data)
+        self.assertEqual(info.data['DOI'], crdoi)
         
 
 
