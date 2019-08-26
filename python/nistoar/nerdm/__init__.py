8ea3bfc513b9be29c1d5a18662914454388d14d0
Ray Plante <raymond.plante@nist.gov>
2019-08-26 05:13:29 -0400

    nerdm module: don't hide validate module with validate function

--- python/nistoar/nerdm/__init__.py
+++ python/nistoar/nerdm/__init__.py
@@ -4,6 +4,6 @@ library for supporting NERDm metadata
 from __future__ import absolute_import
 from .version import __version__
 
-from .validate import validate
+from .validate import validate as validate_nerdm
 from .constants import CORE_SCHEMA_URI, PUB_SCHEMA_URI
 
