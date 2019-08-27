5463c28eae6421a97dff2bac860d822e9d77394c
Ray Plante <raymond.plante@nist.gov>
2019-08-27 17:28:50 -0400

    update testall.py to run new tests

--- scripts/testall.py
+++ scripts/testall.py
@@ -7,8 +7,10 @@ sys.modules['warnings'] = warnings
 basedir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 jqlib = os.path.join(basedir, "jq")
 testdir = os.path.join(jqlib, "tests")
-jqtest = os.path.join(testdir, "test_pod2nerdm.jqt")
-nerdmtest = os.path.join(testdir, "test_podds2resource.py")
+jqtest = [os.path.join(testdir, "test_pod2nerdm.jqt"),
+          os.path.join(testdir, "test_nerdm2pod.jqt")]
+nerdmtest = [os.path.join(testdir, "test_podds2resource.py"),
+             os.path.join(testdir, "test_resource2midaspodds.py")]
 pdltest = os.path.join(basedir, "scripts", "test_pdl2resources.py")
 extest = os.path.join(basedir, "model", "tests", "test_examples.py")
 pydir = os.path.join(basedir, "python")
@@ -18,15 +20,17 @@ print "Executing all tests..."
 print "Executing jq translation library tests..."
 
 status = 0
-notok = os.system("jq -L {0} --run-tests {1}".format(jqlib, jqtest))
-if notok:
+notok = os.system("jq -L {0} --run-tests {1}".format(jqlib, jqtest[0]))
+notok2 = os.system("jq -L {0} --run-tests {1}".format(jqlib, jqtest[1]))
+if notok or notok2:
     print "**ERROR: some or all jq tests have failed"
     status += 1
 
 print "Executing validation tests..."
 
-notok = os.system("python {0}".format(nerdmtest))
-if notok:
+notok  = os.system("python {0}".format(nerdmtest[0]))
+notok2 = os.system("python {0}".format(nerdmtest[1]))
+if notok or notok2:
     print "**ERROR: some or all basic validation tests have failed"
     status += 2
 notok = os.system("python {0}".format(extest))
