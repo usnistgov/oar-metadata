d5f8f4fc2a7f32cbe1903181a146bf127390ae94
Ray Plante <raymond.plante@nist.gov>
2020-07-24 06:34:58 -0400

    nerdm/validate: make RefResolutionError available

--- python/nistoar/nerdm/validate.py
+++ python/nistoar/nerdm/validate.py
@@ -4,7 +4,7 @@ tools for validating NERDm metadata
 from collections import Mapping
 
 import ejsonschema as ejs
-from ejsonschema import ValidationError
+from ejsonschema import ValidationError, RefResolutionError
 
 def get_mdval_flavor(data):
     """
