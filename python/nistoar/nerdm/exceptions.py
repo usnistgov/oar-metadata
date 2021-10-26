bd84ff5dcde8e064e608ca44a35c15711729483f
Ray Plante <raymond.plante@nist.gov>
2021-10-26 07:24:04 -0400

    incorporate src param into Metadata/POD/NERDError

--- python/nistoar/nerdm/exceptions.py
+++ python/nistoar/nerdm/exceptions.py
@@ -6,35 +6,65 @@ from ..base import OARException
 class MetadataError(OARException):
     """
     an exception indicating an error handling metadata
+    :param str msg:          the message describing the error that occurred
+    :param Exception cause:  the exception that originally caught the error; if None, the 
+                               error was discovered by catching an exception
+    :param src:              the file containing the errant metadata; if None, no such 
+                               file source was involved or is known
+    :type src: str or Path
+    :param SysInfoMixin sys: the OAR system that generated the error; if None, the system
+                               will be discerned
     """
-    def __init__(self, msg=None, cause=None, sys=None):
+    def __init__(self, msg=None, cause=None, src=None, sys=None):
         if not msg and not cause:
             msg = "Unknown Metadata Error"
         super(MetadataError, self).__init__(msg, cause, sys)
+        self.source = src
+
+class PODError(MetadataError):
+    """
+    an exception indicating an error handling POD metadata
+    """
+    def __init__(self, msg=None, cause=None, src=None, sys=None):
+        if not msg and not cause:
+            msg = "Unknown POD metadata error"
+        super(PODError, self).__init__(msg, cause, src, sys)
+    pass
 
 class NERDError(MetadataError):
     """
     an exception indicating an error handling NERDm metadata
     """
+    """
+    an exception indicating an error handling POD metadata
+    """
+    def __init__(self, msg=None, cause=None, src=None, sys=None):
+        if not msg and not cause:
+            msg = "Unknown NERD metadata error"
+        super(PODError, self).__init__(msg, cause, src, sys)
     pass
 
-class NERDTypeError(MetadataError):
+class NERDTypeError(NERDError):
     """
     an exception indicating that a NERm metadata value is of an invalid or 
     unexpected type.
     """
-    def __init__(self, need=None, got=None, property=None, msg=None, cause=None, sys=None):
+    def __init__(self, need=None, got=None, property=None, msg=None, cause=None, src=None, sys=None):
         """
         create the exception
 
-        :param need str or list/tuple of str:  the value type or types 
-                             that were expected
-        :param got  str or type:  the type actualy found or given
-        :param property str:  the name of the property with the incorrect type
-        :param msg  str:  a message that overrides the default based on 
-                             need and got.
-        :param cause Exception:  an exception that was a result of this 
-                             problem.
+        :param            need:  the value type or types that were expected
+                                 :type need: str or list/tuple of str
+        :param             got:  the type actualy found or given
+                                 :type got: str or type
+        :param str    property:  the name of the property with the incorrect type
+        :param str         msg:  a message that overrides the default based on 
+                                 need and got.
+        :param Exception cause:  an exception that was a result of this 
+                                 problem.
+        :param src:              the file containing the errant metadata; if None, no such 
+                                 file source was involved or is known
+                                 :type src: str or Path
         """
         self.need = need
         self.got = got
